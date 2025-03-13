3966c4795966960f6df48d2634465ce3c93f296b
sofiia-khoptiak <sofiiakhoptiak@gmail.com>
2025-03-13 09:55:44 +0200

    refactored

--- lab1_1.js
+++ lab1_1.js
@@ -1,28 +1,28 @@
 function isInteger(num) {
-    return Number.isInteger(num)
-  }
-  
-  function isPrime(num) {
-    if (num < 2)
+  return Number.isInteger(num)
+}
+
+function isPrime(num) {
+  if (num < 2)
+    return false;
+  const sqrtNum = Math.sqrt(num)
+  for (i = 2; sqrtNum >= i; i++) {
+    if (num % i === 0)
       return false;
-      const sqrtNum = Math.sqrt(num)
-    for(i = 2; sqrtNum >= i; i++) {
-        if(num % i === 0)
-        return false;
-    }
-    return true;
   }
-  
-  function findPrimes(a,b) {
-       let primes = [];
-    for(num = a; num <= b; num++){
-      if(isPrime(num))
-          primes.push(num);
-    }
-      return primes;
+  return true;
+}
+
+function findPrimes(a, b) {
+  let primes = [];
+  for (num = a; num <= b; num++) {
+    if (isPrime(num))
+      primes.push(num);
   }
-  
-  console.log(findPrimes(10,30));
-  console.log(isPrime(17));
-  console.log(isInteger(10));
-  console.log(isInteger(6.7));
+  return primes;
+}
+
+console.log(findPrimes(10, 30));
+console.log(isPrime(17));
+console.log(isInteger(10));
+console.log(isInteger(6.7));
